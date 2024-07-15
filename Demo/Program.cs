22d2d6eda19d1af96b725ce1c5f1009ed37fdf9f
Gamgom29 <abdogamgom2015@gmail.com>
2024-07-15 03:25:38 +0300

    Question 2: Which of the following is NOT a valid access modifier for interface members in C#?

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,8 +4,12 @@
     {
         static void Main(string[] args)
         {
-            #region What is the primary purpose of an interface in C#?
+            #region Q01 : What is the primary purpose of an interface in C#?
             //b) To define a blueprint for a class
+            #endregion
+
+            #region Q02 : Which of the following is NOT a valid access modifier for interface members in C#?
+            // a) private
             #endregion
         }
     }
