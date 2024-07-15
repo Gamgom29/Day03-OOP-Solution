9f1cc193cf06caf4cc619629705dc1836a55ab50
Gamgom29 <abdogamgom2015@gmail.com>
2024-07-15 03:23:53 +0300

    What is the primary purpose of an interface in C#?

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region What is the primary purpose of an interface in C#?
+            //b) To define a blueprint for a class
+            #endregion
         }
     }
 }
